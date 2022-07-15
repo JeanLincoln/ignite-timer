473e725f194c03d2f5c843436829916069ad24b6
Jean Lincoln <jeanlincol@gmail.com>
2022-07-15 16:44:49 -0700

    Defaultlayout component a its styles added

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+    max-width:74rem;
+    height: calc(100vh - 10rem);
+    margin: 5rem auto;
+    padding:2.5rem;
+
+    background: ${(props) => props.theme['gray-800']};
+    border-radius: 8px;
+
+    display: flex;
+    flex-direction: column;
+
+    nav{
+        display:flex;
+        gap:0.5rem;
+
+        a{
+            width:3rem;
+            height:3rem;
+
+            display:flex;
+            justify-content:center;
+            align-items:center;
+
+            color: ${(props) => props.theme['gray-100']};
+
+            transition:all ease .1s;
+
+            border-top:3px solid transparent;
+            border-bottom:3px solid transparent;
+
+            &:hover{
+                padding-bottom:.5rem;
+                border-bottom:3px solid ${(props) => props.theme['green-500']};
+            }
+
+            &.active{
+                color: ${(props) => props.theme['green-500']}
+            }
+        }
+    }
+`
