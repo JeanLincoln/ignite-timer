a397bb5fc2aa6b1549c2017fdb7f7bb698158280
Jean Lincoln <jeanlincol@gmail.com>
2022-07-20 15:04:29 -0700

    NewCycleForm component, creation, ContextAPI added,  all functions and variables migrated, formContext added

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,38 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
+import { useContext } from "react";
+import { CyclesContext } from "../..";
+import { useFormContext } from "react-hook-form";
+
+export function NewCycleForm() {
+    const { activeCycle } = useContext(CyclesContext)
+    const { register } = useFormContext()
+
+    return(
+        <FormContainer>
+            <label htmlFor="task">I will work on</label>
+            <TaskInput 
+                id="text"
+                {...register('task')}
+                placeholder='Give a name for your project' 
+                list="taskSuggestions"
+                disabled={!!activeCycle}
+            />
+            <label 
+                htmlFor="minutesAmount">over
+            </label>
+            <MinutesAmountInput 
+                type="number" 
+                id="minutesAmount"
+                {...register('minutesAmount', {
+                    valueAsNumber:true
+                })}
+                placeholder='00' 
+                step={5}
+                min={1}
+                max={60}
+                disabled={!!activeCycle}
+            />
+            <span>minutes</span>
+        </FormContainer>
+    )
+}
